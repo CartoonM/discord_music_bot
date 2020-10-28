6fba0b28000236f6a3f10b47b0500e4af32ebb6b
Adamtsev Pavel <adamtcevs@gmail.com>
2020-10-28 15:36:02 +0200

    del unused imports

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
 from os import getenv
 
 from dotenv import load_dotenv
-from discord import FFmpegPCMAudio
 from discord.ext import commands
-from discord.utils import get
-from youtube_dl import YoutubeDL
 
 from cogs import Music
 
